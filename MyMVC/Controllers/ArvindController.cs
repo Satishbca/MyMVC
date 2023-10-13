783a045005126503c8b4f49e52d1b42a143be33c
Satishbca <satishyadav.net@gmail.com>
2023-10-13 22:06:45 +0530

    add a cotroller

---
+++ MyMVC/Controllers/ArvindController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMVC.Controllers
+{
+    public class ArvindController : Controller
+    {
+        public IActionResult Index()
+        {
+         
+            //test
+            
+            return View();
+        }
+    }
+}
